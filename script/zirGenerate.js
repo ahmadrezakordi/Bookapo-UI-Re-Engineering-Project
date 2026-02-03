7d0a02ba0199a5c42da31dbab25cc29233efa3e0
Ahmadreza <ahmadrk.9747@gmail.com>
2026-02-03 21:19:20 +0330

    Update zirGenerate.js

--- script/zirGenerate.js
+++ script/zirGenerate.js
@@ -4,7 +4,7 @@ function generateCatItems(holderID,jsonDataItem){
     it.className = "row1";
 
     let pic = document.createElement('img');
-    pic.src = "D:/Sari/Bookapo/images/i" + jsonDataItem.catIcon;
+    pic.src = "../images/i" + jsonDataItem.catIcon;
 
     let li = document.createElement('p')
     li.innerHTML = jsonDataItem.catName;
@@ -38,3 +38,4 @@ document.getElementById(holderID).innerHTML = "";
 	generateCatItems(holderID,jsonData[i]);
 	}
 }
+
